833a0778908d0269d8f8eca2de011d5b5d1a8da6
xing <xing@anakinfoxe>
2014-07-01 22:20:37 -0700

    Cleaned up.

--- src/main/java/edu/csupomona/ir/solrapplication/Searcher.java
+++ src/main/java/edu/csupomona/ir/solrapplication/Searcher.java
@@ -28,7 +28,6 @@ import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.HttpSolrServer;
 import org.apache.solr.client.solrj.response.QueryResponse;
-import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrDocumentList;
 
 /**
